d5bdf6325a573228ce5ca00e19e0d15a83efc65b
Jaden Ngo <jadenngo13@gmail.com>
2018-05-21 19:29:03 -0700

    firstlineofcode

---
+++ code.py
@@ -0,0 +1,11 @@
+names = {
+'jaden' : "Is awesome",
+'marcus' : "I have a slow car",
+'jesse' : "I like eating jaden's snacks",
+'michael' : "I can burp"
+}
+name = input("Enter name:" )
+if name in names.keys():
+    print("My name is", name, "and", names[name])
+else:
+    print("Sorry I dont know you :/")
